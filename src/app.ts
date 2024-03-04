52cb21232b3b92cec9488dc7a780b2c5366bf122
YDan08 <dasyamashita@gmail.com>
2024-03-04 15:01:09 -0300

    feat: use dotenv

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,10 @@ import "reflect-metadata"
 import { buildSchema } from "type-graphql"
 import { ProductResolver } from "./product"
 import { db } from "./db/db"
+import { config } from "dotenv"
 
 const bootstrap = async () => {
+	config()
 	await db()
 
 	const schema = await buildSchema({
